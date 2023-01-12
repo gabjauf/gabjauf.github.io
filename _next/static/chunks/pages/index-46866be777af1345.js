7408ee75647151f96536773dd9b97f6aeb9f85ac
gabjauf <gabjauf@users.noreply.github.com>
2023-01-12 09:46:54 +0000

    deploy: 63e489401d346e7da4b010abeb91306b5d4c23ff

---
+++ _next/static/chunks/pages/index-46866be777af1345.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[405],{8312:function(n,u,t){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return t(4186)}])},4186:function(n,u,t){"use strict";t.r(u);var e=t(5893);u.default=function(){return(0,e.jsx)("div",{children:"Welcome to Next.js!"})}}},function(n){n.O(0,[774,888,179],function(){return n(n.s=8312)}),_N_E=n.O()}]);
