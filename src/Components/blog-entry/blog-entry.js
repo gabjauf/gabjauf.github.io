5086923a75e718d058fe316f01de963c96bbca89
Gabriel Jauffret <gabjauf@hotmail.fr>
2023-01-12 10:20:51 +0100

    change from react to nextjs

--- src/Components/blog-entry/blog-entry.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export class BlogEntry extends React.Component {
-  render() {
-    return <h1>This is a blog entry</h1>
-  }
-}
--- src/views/blog/blog.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export class Articles extends React.Component {
-  render() {
-    return <h1>Blog</h1>
-  }
-}
